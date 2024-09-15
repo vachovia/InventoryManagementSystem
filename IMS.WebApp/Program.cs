18951bf8ea17493e1f3849d8b294925f9613101d
Vladislav Petrosyan <petrosyanv@dynacare.ca>
2024-09-15 16:15:07 -0400

    Add project files.

---
+++ IMS.WebApp/Program.cs
@@ -0,0 +1,24 @@
+using IMS.WebApp.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>();
+
+app.Run();
